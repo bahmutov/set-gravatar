6a687c71aefc39121f0c7e764470e8cdf1fc4e1e
Gleb Bahmutov <gleb@kensho.com>
2014-01-25 16:46:17 -0500

    added initial gulp

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+
+gulp.task('lint', function() {
+  gulp.src('*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('default', function () {
+  gulp.run('lint');
+});
