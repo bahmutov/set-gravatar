e265bff180f2fe4cddbc8189b6e59deded72a521
Gleb Bahmutov <gleb@kensho.com>
2014-01-26 16:30:15 -0500

    gettinglist of addresses

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 
 gulp.task('lint', function() {
-  gulp.src('*.js')
+  gulp.src(['*.js', 'test/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
--- test/access.js
+++ test/access.js
@@ -3,30 +3,49 @@
 var verify = require('check-types').verify;
 var crypto = require('crypto');
 var xmlrpc = require('xmlrpc');
+require('console.json');
 var client;
 
 var email = process.env.GRAVATAR_EMAIL;
 verify.unemptyString(email, 'missing process.env.GRAVATAR_USER');
 var hash = crypto.createHash('md5').update(email.toLowerCase().trim()).digest('hex');
 verify.unemptyString(hash, 'could not generate hash from email ' + email);
-console.log('email hash', hash);
+console.json({
+  email: email,
+  hash: hash
+});
 
 var password = process.env.GRAVATAR_PASSWORD;
 verify.unemptyString(password, 'missing process.env.GRAVATAR_PASSWORD');
-console.log('password', password);
-
 
 gt.module('gravatar api access', {
   setupOnce: function () {
-    client = xmlrpc.createSecureClient('https://secure.gravatar.com/xmlrpc?user=' + hash);
+    var secureClientOptions = {
+      host: 'secure.gravatar.com',
+      path: '/xmlrpc?user=' + hash,
+      port: 443
+    };
+    client = xmlrpc.createSecureClient(secureClientOptions);
   }
 });
 
 var delay = 15000;
 
 gt.async('grav.test', function () {
-  client.methodCall('grav.test', [password], function (error, value) {
-    console.log('grav.test returned', error, 'value', value);
+  client.methodCall('grav.test', [{ password: password }], function (error, value) {
+    if (error) throw error;
+    gt.ok(value, 'a value is returned');
     gt.start();
-  })
+  });
 }, delay);
+
+gt.async('grav.addresses', function () {
+  client.methodCall('grav.addresses', [{ password: password }], function (error, addresses) {
+    if (error) throw error;
+    console.json(addresses);
+    gt.object(addresses, 'list of addresses is an object');
+    gt.ok(Object.keys(addresses).length > 0, 'there are several addresses');
+    gt.start();
+  });
+}, delay);
+
